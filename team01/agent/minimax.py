541d05f77ef73cb059c6185a09bfbc0dfa3eb756
emma.maertz <emma.maertz@gmail.com>
2026-09-20 01:58:04 -0400

    ok, minimax now has alpha-beta pruning and i am going to bed. still needs depth limiting, tho

--- team01/agent/minimax.py
+++ team01/agent/minimax.py
@@ -4,7 +4,8 @@ from team01.agent.actions import AgentAction
 from team01.agent.black_board import BlackBoardValue
 
 # TODO: most typing is temporary
-# TODO: I FORGOT THE KRIFFING ALPHA-BETA PRUNING OSIK OSIK OSIK
+# TODO: might want to pass around [v, state], where <state> is the closest state 
+#       that leads to v, so it's easier to identify it later
 
 class Minimax():
     """
@@ -22,11 +23,12 @@ class Minimax():
         self.curr_depth = 0 # initialize current depth, alpha, and beta
         alpha = -math.inf
         beta  =  math.inf
-        return self.get_action(self.min_val(self.result(state), alpha, beta)) # aka return the index of the max choice returned by the min_val() fn given the result of a state and action
+        return self.get_action(self.min_val(state, alpha, beta)) # aka return the index of the max choice returned by the min_val() fn given the result of a state and action
 
 
     def max_val(self, state, alpha: float, beta: float): # returns a utility value (optimal cell)
-        '''returns the maximum value possible to achieve from the actions available for state'''
+        """ Returns the maximum value possible to achieve from the actions available for state """
+        
         if self.terminal_test(state):
             return self.utility(state)
         
@@ -35,13 +37,19 @@ class Minimax():
             return self.utility(state)
         
         v = - math.inf
-        for a in self.actions(state): 
+        
+        for a in self.actions(state): # update v to max value of possible actions, pruning if necessary
             v = max(v, self.min_val(self.result(state, a), a))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+            
         return v
     
 
     def min_val(self, state, alpha: float, beta: float): # returns a utility value (optimal cell)
-        '''returns the maximum value possible to achieve from the actions available for state'''
+        """ Returns the minimum value possible to achieve from the actions available for state """
+        
         if self.terminal_test(state):
             return self.utility(state)
         
@@ -50,30 +58,40 @@ class Minimax():
             return self.utility(state)
         
         v = math.inf
+        
         for a in self.actions(state): 
             v = min(v, self.max_val(self.result(state, a), a)) 
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+            
         return v
     
     
     ### Supporting Functions
-    def terminal_test(self, state) -> bool:
-            if self.actions(state) == []: # TODO: replace with whatever the terminal test actually is
-                return True
-            return False
+    def terminal_test(self, s) -> bool:
+        """ Returns true if the state is a utility node, and false otherwise """
+        if self.actions(s) == []: # TODO: replace with whatever the terminal test actually is
+            return True
+        return False
     
     
-    def actions(self, state) -> List[AgentAction]: # TODO: update with actual method for getting possible actions from a state
-            return [AgentAction(0,0,False)]
+    def actions(self, s) -> List[AgentAction]: # TODO: update with actual method for getting possible actions from a state
+        """ Returns a list of all possible actions for the given state """
+        return [AgentAction(0,0,False)]
         
         
-    def result(self, state): # TODO: update with actual method of getting the result of taking an action a at state
-        return state
+    def result(self, s, a: AgentAction): # TODO: update with actual method of getting the result of taking an action a at state
+        """ Returns the state (or node) _s'_ that results from taking action _a_ in state (or node) _s_ """
+        return s
     
     
-    def utility(self, state) -> float:  # TODO: update with whatever the actual utility ends up being
+    def utility(self, s) -> float:  # TODO: update with whatever the actual utility ends up being
+        """ Returns the utility of node _s_"""
         return 0.0
     
     
-    def get_action(self, val) -> AgentAction: # TODO: update with correct method of getting index
+    def get_action(self, val) -> AgentAction: # TODO: update with correct method of getting index. this is probably not the way to do this.
+        """ Returns the action associated with the value _val_ """
         return AgentAction(0,0,False)
     
